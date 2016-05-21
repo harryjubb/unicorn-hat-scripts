6b05ea66c4602396aeb7031356b37b80d14be66e
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-21 22:53:02 +0100

    Added warm and cool lights.

---
+++ coollight.py
@@ -0,0 +1,7 @@
+import unicorntools
+import time
+
+unicorntools.show_all_pixels(125, 125, 255, 0.5)
+
+while 1:
+    time.sleep(1)
---
+++ warmlight.py
@@ -0,0 +1,7 @@
+import unicorntools
+import time
+
+unicorntools.show_all_pixels(255, 86, 86, 0.5)
+
+while 1:
+    time.sleep(1)
